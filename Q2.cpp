9ecb8ab0d63289e97abfceeff31dec941ef4a4ad
Mohamedelsaid584 <165983620+Mohamedelsaid584@users.noreply.github.com>
2024-04-04 21:25:25 +0200

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i , pas ;
+
+while(true)
+	{
+	
+	cout<<"enter the password \n";
+	cin>>pas;
+	if(pas == 1999){
+		cout<<"Correct\n";
+		break;
+		
+	}
+	else 
+	{
+		cout<<"Wrong\n";
+	}
+	
+
+}
+} 
